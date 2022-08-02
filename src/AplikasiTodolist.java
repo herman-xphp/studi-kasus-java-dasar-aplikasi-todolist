96dd0beb82dd6cad26b7d8aa1649edd03065faa1
herman-xphp <hermanxphp@gmail.com>
2022-08-02 21:25:53 +0800

    impl bussines logic menambahkan todolist, membuat unit test menambahkan todolist

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -4,7 +4,7 @@ public class AplikasiTodolist {
 
   public static void main(String[] args) {
 
-    testShowTodoList();
+    testAddTodoList();
 
   }
 
@@ -34,8 +34,41 @@ public class AplikasiTodolist {
   /**
    * Menambah todo list
    */
-  public static void addTodoList() {
+  public static void addTodoList(String todo) {
 
+  // cek apakah model penuh
+    var isFull = true;
+    for (String s : model) {
+      if (s == null) {
+        isFull = false;
+        break;
+      }
+    }
+
+    // jika penuh, risize ukuran model 2x lipat
+   if (isFull){
+     var temp = model;
+     model = new String[model.length * 2];
+
+     System.arraycopy(temp, 0, model, 0, temp.length);
+   }
+
+  // tambahkan ke posisi array yang NULL
+    for (var i = 0; i < model.length; i++){
+      if (model[i] == null){
+        model[i] = todo;
+        break;
+      }
+    }
+  }
+
+  // Unit test method addTodoList()
+  public static void testAddTodoList(){
+    for (int i = 0; i < 25; i++) {
+      addTodoList("Contoh todo ke. " + i);
+    }
+
+    showTodoList();
   }
 
   /**
